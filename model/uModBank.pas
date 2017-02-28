6db553a11064fb95dfeafce38d24b7962d4cdf88
bagusprasojo <bagusprasojo@gmail.com>
2017-02-28 13:51:48 +0700

    menambahkan uModBank.pas

---
+++ model/uModBank.pas
@@ -0,0 +1,28 @@
+unit uModBank;
+
+interface
+uses
+  uModApp, uModAccount;
+
+type
+  TModBank = class(TModApp)
+  private
+    FBANK_ADDRESS: string;
+    FBANK_BRANCH: string;
+    FBANK_CODE: string;
+    FBANK_DESCRIPTION: string;
+    FBANK_NAME: string;
+    FBANK_REKENING: TModRekening;
+  published
+    property BANK_ADDRESS: string read FBANK_ADDRESS write FBANK_ADDRESS;
+    property BANK_BRANCH: string read FBANK_BRANCH write FBANK_BRANCH;
+    property BANK_CODE: string read FBANK_CODE write FBANK_CODE;
+    property BANK_DESCRIPTION: string read FBANK_DESCRIPTION write
+        FBANK_DESCRIPTION;
+    property BANK_NAME: string read FBANK_NAME write FBANK_NAME;
+    property BANK_REKENING: TModRekening read FBANK_REKENING write FBANK_REKENING;
+  end;
+
+implementation
+
+end.
