b60a0b4f1191b7e992298d73d144bcaae7e4e622
bagusprasojo <bagusprasojo@gmail.com>
2017-02-28 11:31:04 +0700

    test commit dengan git delphi

--- model/uModAccount.pas
+++ model/uModAccount.pas
@@ -7,6 +7,10 @@ uses
 type
   TModRekening = class(TModApp)
 
+  private
+    FRek_Code: string;
+  published
+    property Rek_Code: string read FRek_Code write FRek_Code;
   end;
 
 implementation
