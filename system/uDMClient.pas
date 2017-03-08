52a841d171ffe94d3aa94a92296833baa6d23479
bagusprasojo <bagusprasojo@gmail.com>
2017-03-08 11:02:19 +0700

    menambahkan property yang dipakai untuk mengkases method2 di server

--- system/uDMClient.pas
+++ system/uDMClient.pas
@@ -10,13 +10,16 @@ type
     RestConn: TDSRestConnection;
     procedure DataModuleCreate(Sender: TObject);
   private
+    FCrudClient: TCrudClient;
     FDSProviderClient: TDSProviderClient;
     FInstanceOwner: Boolean;
+    function GetCrudClient: TCrudClient;
     function GetDSProviderClient: TDSProviderClient;
     function GetInstanceOwner: Boolean;
     property InstanceOwner: Boolean read GetInstanceOwner write FInstanceOwner;
     { Private declarations }
   public
+    property CrudClient: TCrudClient read GetCrudClient write FCrudClient;
     property DSProviderClient: TDSProviderClient read GetDSProviderClient write
         FDSProviderClient;
     { Public declarations }
@@ -34,6 +37,14 @@ implementation
 procedure TDMClient.DataModuleCreate(Sender: TObject);
 begin
   //
+end;
+
+function TDMClient.GetCrudClient: TCrudClient;
+begin
+  if not Assigned(FCrudClient) then
+    FCrudClient := TCrudClient.Create(RestConn, InstanceOwner);
+
+  Result := FCrudClient;
 end;
 
 function TDMClient.GetDSProviderClient: TDSProviderClient;
