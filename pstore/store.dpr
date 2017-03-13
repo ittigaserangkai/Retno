2d712323b90f2928216290a76c60a8e5f9a413b3
arifwib <arifwib@gmail.com>
2017-03-13 10:41:57 +0700

    tambahkan docs progress report development (excel)

--- pstore/store.dpr
+++ pstore/store.dpr
@@ -54,9 +54,9 @@ uses
   ufrmSearchTypeMember in 'module\customer_support\ufrmSearchTypeMember.pas' {frmDialogSearchTypeMember},
   ufrmPopupFamilyMember in 'module\customer_support\ufrmPopupFamilyMember.pas' {frmPopupFamilyMember},
   ufrmPopupPersonalMember in 'module\customer_support\ufrmPopupPersonalMember.pas' {frmPopupPersonalMember},
-  uInterface in '..\system\uInterface.pas',
   ufrmGudang in 'module\inventory\ufrmGudang.pas' {frmGudang},
-  ufrmDialogGudang in 'module\inventory\ufrmDialogGudang.pas' {frmDialogGudang};
+  ufrmDialogGudang in 'module\inventory\ufrmDialogGudang.pas' {frmDialogGudang},
+  uInterface in '..\system\uInterface.pas';
 
 {$R *.res}
 
