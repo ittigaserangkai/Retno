414201d7e5d81d59086cb2aae8a6360c85876b1d
fmagusta <fmagusta@tigaserangkai.co.id>
2017-04-04 09:17:04 +0700

    -

--- module/customer_support/ufrmDialogPersonalMember.pas
+++ module/customer_support/ufrmDialogPersonalMember.pas
@@ -13,8 +13,6 @@ uses
   Datasnap.DBClient;
 
 type
-  TFormMode = (fmAdd, fmEdit);
-
   TModul = (mPopUpPersonalMember, mDialogMemberShip);
   
   TfrmDialogPersonalMember = class(TfrmMasterDialog, iCrudable)
