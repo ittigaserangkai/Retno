a7d2ccd0674531c158900414e9bd7022c8a987f9
bagusprasojo <bagusprasojo@gmail.com>
2017-05-16 07:50:56 +0700

    GR

---
+++ model/uModelHelper.pas
@@ -0,0 +1,43 @@
+unit uModelHelper;
+
+interface
+uses
+  uModApp, uModPO, uModSO,uModSuplier,uModBarang, uModSatuan,
+  System.SysUtils, uDMClient;
+
+type
+  TModPOItemHelper = class helper for  TModPOItem
+  public
+    procedure LoadBarang;
+    procedure LoadUOM;
+  end;
+
+  TModPOHelper = class helper for  TModPO
+  public
+    procedure LoadPO_SUPPLIER_MERCHAN_GRUP;
+    procedure LoadSO;
+
+  end;
+implementation
+
+procedure TModPOHelper.LoadPO_SUPPLIER_MERCHAN_GRUP;
+begin
+  Self.PO_SUPPLIER_MERCHAN_GRUP := TModSuplierMerchanGroup(DMClient.CrudClient.Retrieve(TModSuplierMerchanGroup.ClassName, Self.PO_SUPPLIER_MERCHAN_GRUP.ID));
+end;
+
+procedure TModPOHelper.LoadSO;
+begin
+  Self.PO_SO := TModSO(DMClient.CrudClient.RetrieveSingle(TModSO.ClassName, Self.PO_SO.ID));
+end;
+
+procedure TModPOItemHelper.LoadBarang;
+begin
+  Self.POD_BARANG := TModBarang(DMClient.CrudClient.Retrieve(TModBarang.ClassName, Self.POD_BARANG.ID));
+end;
+
+procedure TModPOItemHelper.LoadUOM;
+begin
+  Self.POD_UOM := TModSatuan(DMClient.CrudClient.Retrieve(TModSatuan.ClassName,Self.POD_UOM.ID));
+end;
+
+end.
