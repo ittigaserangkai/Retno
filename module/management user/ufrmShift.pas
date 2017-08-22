c6b3f78913826e3b2675b627dc00d65554e635aa
agustono <agustono@assalaamhypermarket.co.id>
2017-08-22 14:31:55 +0700

    -

---
+++ module/management user/ufrmShift.pas
@@ -0,0 +1,46 @@
+unit ufrmShift;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, ufrmMasterBrowse, cxGraphics,
+  cxControls, cxLookAndFeels, cxLookAndFeelPainters, dxBarBuiltInMenu, cxStyles,
+  cxCustomData, cxFilter, cxData, cxDataStorage, cxEdit, cxNavigator, Data.DB,
+  cxDBData, cxContainer, Vcl.ComCtrls, dxCore, cxDateUtils, Vcl.Menus,
+  System.Actions, Vcl.ActnList, ufraFooter4Button, Vcl.StdCtrls, cxButtons,
+  cxTextEdit, cxMaskEdit, cxDropDownEdit, cxCalendar, cxLabel, cxGridLevel,
+  cxClasses, cxGridCustomView, cxGridCustomTableView, cxGridTableView,
+  cxGridDBTableView, cxGrid, cxPC, Vcl.ExtCtrls, uDXUtils;
+
+type
+  TfrmShift = class(TfrmMasterBrowse)
+    cxGridViewColumn1: TcxGridDBColumn;
+    cxGridViewColumn2: TcxGridDBColumn;
+    cxGridViewColumn3: TcxGridDBColumn;
+    cxGridViewColumn4: TcxGridDBColumn;
+  private
+    { Private declarations }
+  protected
+  public
+    procedure RefreshData; override;
+    { Public declarations }
+  end;
+
+var
+  frmShift: TfrmShift;
+
+implementation
+
+{$R *.dfm}
+
+uses uDMClient;
+
+procedure TfrmShift.RefreshData;
+begin
+  inherited;
+  cxGridView.LoadFromDS(
+    DMClient.DSProviderClient.Shift_GetDSOverview(),Self,false);
+end;
+
+end.
