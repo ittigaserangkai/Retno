dc6c957098268b311b85c3e5bad3aac79dbe2240
Bask <babaz.kull@gmail.com>
2018-02-08 08:01:50 +0700

    cus

--- module/finance/ufrmCashIn.dfm
+++ module/finance/ufrmCashIn.dfm
@@ -9,62 +9,69 @@ inherited frmCashIn: TfrmCashIn
   inherited pnlBody: TPanel
     Width = 691
     Height = 251
-    ExplicitWidth = 120
+    ExplicitWidth = 691
+    ExplicitHeight = 251
     inherited pgcBrowse: TcxPageControl
       Width = 689
       Height = 249
-      ExplicitWidth = 118
+      ExplicitWidth = 689
+      ExplicitHeight = 249
       ClientRectBottom = 248
       ClientRectRight = 688
       inherited tsBrowse: TcxTabSheet
         ExplicitLeft = 1
         ExplicitTop = 1
-        ExplicitWidth = 116
-        ExplicitHeight = 310
+        ExplicitWidth = 687
+        ExplicitHeight = 247
         inherited cxGrid: TcxGrid
           Width = 687
           Height = 247
-          ExplicitWidth = 116
+          ExplicitWidth = 687
+          ExplicitHeight = 247
         end
       end
     end
   end
   inherited pnlHeader: TPanel
     Width = 691
-    ExplicitWidth = 120
+    ExplicitWidth = 691
     inherited lblFilterData: TcxLabel
       Left = 337
-      ExplicitLeft = -234
+      Visible = True
+      ExplicitLeft = 337
       AnchorY = 17
     end
     inherited dtAwalFilter: TcxDateEdit
       Left = 398
-      ExplicitLeft = -173
+      Visible = True
+      ExplicitLeft = 398
       ExplicitHeight = 23
     end
     inherited dtAkhirFilter: TcxDateEdit
       Left = 514
-      ExplicitLeft = -57
+      Visible = True
+      ExplicitLeft = 514
       ExplicitHeight = 23
     end
     inherited btnSearch: TcxButton
       Left = 609
-      ExplicitLeft = 38
+      ExplicitLeft = 609
     end
     inherited lblsdFilter: TcxLabel
       Left = 490
-      ExplicitLeft = -81
+      Visible = True
+      ExplicitLeft = 490
       AnchorY = 17
     end
   end
   inherited fraFooter4Button1: TfraFooter4Button
     Top = 284
     Width = 691
-    ExplicitTop = -56
-    ExplicitWidth = 120
+    ExplicitTop = 284
+    ExplicitWidth = 691
     inherited pnlFooter: TPanel
       Width = 691
-      ExplicitWidth = 120
+      ExplicitWidth = 691
       inherited btnAdd: TcxButton
         Action = actAdd
       end
@@ -77,7 +84,7 @@ inherited frmCashIn: TfrmCashIn
       inherited btnClose: TcxButton
         Left = 614
         Action = actClose
-        ExplicitLeft = 43
+        ExplicitLeft = 614
       end
       inherited cxButton1: TcxButton
         Action = actExport
@@ -85,10 +92,11 @@ inherited frmCashIn: TfrmCashIn
     end
     inherited pnlShortCut: TPanel
       Width = 691
-      ExplicitWidth = 120
+      ExplicitWidth = 691
       inherited lbEscape: TLabel
         Left = 617
         Height = 17
+        ExplicitLeft = 617
       end
     end
   end
--- module/finance/ufrmDialogCashIn.pas
+++ module/finance/ufrmDialogCashIn.pas
@@ -14,7 +14,8 @@ uses
   cxTextEdit, cxMaskEdit, cxDropDownEdit, cxCalendar, StdCtrls, cxLabel,
   cxButtons, cxGridLevel, cxPC, ComCtrls, cxMemo, cxCurrencyEdit, cxButtonEdit,
   cxListView, cxLookupEdit, cxDBLookupEdit, cxDBExtLookupComboBox,
-  cxSpinEdit, dxCore, cxDateUtils, dxBarBuiltInMenu, cxNavigator, uDMClient;
+  cxSpinEdit, dxCore, cxDateUtils, dxBarBuiltInMenu, cxNavigator, uDMClient,
+  uModBankCashIn, uModOrganization;
 
 type
   TfrmDialogCashIn = class(TfrmMasterDialog)
@@ -68,7 +69,24 @@ type
     cxGridColGridDBTableChequeColumn4: TcxGridDBColumn;
     edNamaOrganization: TcxButtonEdit;
     cxGridColGridTableNewAPColumn6: TcxGridColumn;
+    procedure FormCreate(Sender: TObject);
+    procedure actSaveExecute(Sender: TObject);
+    procedure beBusinessPartnerExit(Sender: TObject);
+    procedure beBusinessPartnerPropertiesButtonClick(Sender: TObject;
+      AButtonIndex: Integer);
+    procedure beBusinessPartnerPropertiesEditValueChanged(Sender: TObject);
   private
+    FModCashIn: TModBankCashIn;
+    FModOrganization: TModOrganization;
+    function GetModCashIn: TModBankCashIn;
+    procedure initLookUp;
+    procedure LookUpOrganization;
+    procedure SetOrganization(Aorg: TModOrganization);
+    procedure UpdateData;
+    function ValidateData: boolean;
+    property ModCashIn: TModBankCashIn read GetModCashIn write FModCashIn;
+    property ModOrganization: TModOrganization read FModOrganization write
+        FModOrganization;
     { Private declarations }
   public
     { Public declarations }
@@ -79,6 +97,133 @@ var
 
 implementation
 
+uses
+  uModBank, uAppUtils, uDXUtils, ufrmLookupOrganization, uModelHelper;
+
 {$R *.dfm}
+
+procedure TfrmDialogCashIn.beBusinessPartnerPropertiesButtonClick(
+  Sender: TObject; AButtonIndex: Integer);
+begin
+  inherited;
+  LookUpOrganization;
+end;
+
+procedure TfrmDialogCashIn.beBusinessPartnerPropertiesEditValueChanged(
+  Sender: TObject);
+begin
+  inherited;
+ 
+
+
+
+//  ModOrganization := Tcrud
+end;
+
+procedure TfrmDialogCashIn.FormCreate(Sender: TObject);
+begin
+  inherited;
+  initLookUp;
+end;
+
+procedure TfrmDialogCashIn.actSaveExecute(Sender: TObject);
+begin
+  inherited;
+  if not ValidateData then  exit;
+
+  UpdateData;
+
+  if DMClient.CrudClient.SaveToDB(ModCashIn) Then
+    TAppUtils.Information('Berhasil Simpan');
+
+end;
+
+procedure TfrmDialogCashIn.beBusinessPartnerExit(Sender: TObject);
+begin
+  inherited;
+   freeandnil (FModOrganization);
+//  ModOrganization := DMClient.CrudClient.RetrieveByCode(TModOrganization.ClassName, beBusinessPartner.Text) as TModOrganization;
+  ModOrganization := TCRUDObj.RetrieveCode<TModOrganization>(beBusinessPartner.Text);
+  //format :
+  //model = TCrudObj.Retrieve<Classobject>(ID);
+  if ModOrganization.ID = '' then
+    TAppUtils.Warning('Kode Org ' + beBusinessPartner.Text + ' tidak ditemukan');
+
+  SetOrganization(ModOrganization);
+end;
+
+function TfrmDialogCashIn.GetModCashIn: TModBankCashIn;
+begin
+if not assigned (fModCashIn) then
+  fModCashIn := TModBankCashIn.create;
+  Result := FModCashIn;
+end;
+
+procedure TfrmDialogCashIn.initLookUp;
+var
+  LCDSBank: TDataset;
+begin
+   LCDSBank := DMClient.DSProviderClient.Bank_GetDSLookup();
+   cbBank.Properties.LoadFromDS(LCDSBank, 'BANK_ID', 'BANK_NAME',['BANK_ID'], self);
+end;
+
+procedure TfrmDialogCashIn.LookUpOrganization;
+begin
+  freeandnil (fModOrganization);
+  ModOrganization := TfrmLookupOrganization.Lookup(1);
+  SetOrganization(ModOrganization);
+end;
+
+procedure TfrmDialogCashIn.SetOrganization(Aorg: TModOrganization);
+begin
+if Aorg = nil then
+    exit;
+  beBusinessPartner.Text := Aorg.ORG_Code;
+  edNamaOrganization.Text := Aorg.ORG_Name;
+end;
+
+procedure TfrmDialogCashIn.UpdateData;
+begin
+ModCashIn.BCI_Keterangan := memDesc.Text;
+ModCashIn.BCI_Bank := TModBank.CreateID(vartostr(cbBank.EditValue));
+ModCashIn.BCI_NoBukti := edReceiveNum.Text;
+ModCashIn.BCI_Organization := ModOrganization;
+ModCashIn.BCI_Ref := edRefNum.Text;
+ModCashIn.BCI_Tanggal := deReceiveDate.Date;
+ModCashIn.BCI_Total := ceNominal.Value;
+//ModCashIn.BCI_Total_APNEW :=
+//ModCashIn.BCI_Total_AR := lvSumary.Items[0] ;
+
+
+
+  // TODO -cMM: TfrmDialogCashIn.UpdateData default body inserted
+end;
+
+function TfrmDialogCashIn.ValidateData: boolean;
+begin
+  Result := False;
+  if deReceiveDate.Text ='' then
+  begin TAppUtils.Warning('Tanggal Tidak Boleh Kosong');
+  exit;
+  end;
+
+  if beBusinessPartner.Text ='' then
+  begin TAppUtils.Warning('Kode Tidak Boleh Kosong');
+  exit;
+  end;
+
+  if cbBank.Text ='' then
+  begin TAppUtils.Warning('Bank Tidak Boleh Kosong');
+  exit;
+  end;
+
+  if ceNominal.Value <= 0 then
+  begin TAppUtils.Warning('Nominal Tidak Boleh Nol');
+  exit;
+  end;
+
+Result := True;
+  // TODO -cMM: TfrmDialogCashIn.ValidateData default body inserted
+end;
 
 end.
